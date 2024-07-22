3b9d0a1b0ffc9b571b300b01e72eeda61aa41314
Dennis Muga <dennis.mutethia1@accesscfa.com>
2024-07-22 17:21:03 +0300

    Update dashboard.py

--- utils/dashboard.py
+++ utils/dashboard.py
@@ -1,10 +1,11 @@
 from flask import render_template, request
-from flask_login import current_user
+from flask_login import current_user, login_required
 
 class Dashboard():
     def __init__(self, db): 
         self.db = db
-        
+    
+    @login_required  
     def __call__(self):
         shop = self.db.get_shop_by_id(current_user.shop_id) 
         company = self.db.get_company_by_id(shop.company_id)

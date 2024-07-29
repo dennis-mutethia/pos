934f3407778045b500612a3b59c2f3c3fc6a85a8
Dennis Muga <dennis.mutethia1@accesscfa.com>
2024-07-29 22:17:49 +0300

    charts1 & 2

--- utils/dashboard.py
+++ utils/dashboard.py
@@ -1,3 +1,4 @@
+import random
 from datetime import datetime
 from flask import render_template, request
 from flask_login import current_user
@@ -11,7 +12,65 @@ from utils.pos.bills import Bills
 class Dashboard():
     def __init__(self, db): 
         self.db = db
-        
+    
+    def get_sales_per_item(self, report_date):
+        self.db.ensure_connection()
+        with self.db.conn.cursor() as cursor:
+            query = """
+            SELECT item_name, SUM(qty) sold
+            FROM bill_entries 
+            WHERE DATE(created_at) = DATE(%s) AND shop_id = %s
+            AND bill_id>0 AND qty>0
+            GROUP BY item_name            
+            """
+            params = [report_date, current_user.shop.id]
+            
+            cursor.execute(query, tuple(params))
+            data = cursor.fetchall()
+            items = []
+            qtys = []
+            bgcolors = []
+            for datum in data:  
+                items.append(f"'{datum[0]}'")
+                qtys.append(datum[1])
+                bgcolors.append(f"'rgb({random.randint(1, 255)},{random.randint(1, 255)},{random.randint(1, 255)})'")
+
+            return items, qtys, bgcolors
+    
+    def get_sales_and_expenses(self, report_date):
+        self.db.ensure_connection()
+        with self.db.conn.cursor() as cursor:
+            query = """
+            WITH exp AS(
+                SELECT date, SUM(amount) total_expenses
+                FROM expenses
+                GROUP BY date
+            ),
+            sales AS(
+                SELECT DATE(created_at) AS report_date, SUM(total) AS total_sales
+                FROM bills
+                WHERE DATE(created_at) BETWEEN DATE(%s) - INTERVAL '30 days' AND DATE(%s) AND shop_id=%s
+                GROUP BY report_date
+            )
+            SELECT report_date, total_sales, COALESCE(total_expenses,0) AS total_expenses
+            FROM sales
+            LEFT JOIN exp ON exp.date=sales.report_date         
+            """
+            params = [report_date, report_date, current_user.shop.id]
+            
+            cursor.execute(query, tuple(params))
+            data = cursor.fetchall()
+            dates = []
+            sales_all = []
+            expenses_all = []
+            for datum in data:  
+                dates.append(f"'{datum[0]}'")
+                sales_all.append(datum[1])
+                expenses_all.append(datum[2])
+
+            return dates, sales_all, expenses_all
+          
+                 
     def __call__(self):
         current_date = datetime.now().strftime('%Y-%m-%d')
         report_date = current_date
@@ -27,8 +86,12 @@ class Dashboard():
         total_expenses = Expenses(self.db).get_total(report_date)
         total_capital, total_stock = StockTake(self.db).get_total(report_date)
         total_unpaid_bills = Bills(self.db).get_total_unpaid_bills()
+        items, qtys, bgcolors = self.get_sales_per_item(report_date)
+        dates, sales_all, expenses_all = get_sales_and_expenses(report_date)
          
         return render_template('dashboard/index.html', page_title='Dashboard', helper=Helper(),
                                report_date=report_date,
                                total_cost=total_cost, total_sales=total_sales, total_expenses=total_expenses,
-                               total_capital=total_capital, total_stock=total_stock, total_unpaid_bills=total_unpaid_bills )
+                               total_capital=total_capital, total_stock=total_stock, total_unpaid_bills=total_unpaid_bills,
+                               items=items, qtys=qtys, bgcolors=bgcolors, dates=dates, sales_all=sales_all, expenses_all=expenses_all
+                               )

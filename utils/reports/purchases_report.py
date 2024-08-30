6b579a1b947ab7a3bfa99f47159437ddaca3862e
Dennis Muga <dennis.mutethia1@accesscfa.com>
2024-08-30 12:18:58 +0300

    filters

--- utils/reports/purchases_report.py
+++ utils/reports/purchases_report.py
@@ -90,18 +90,25 @@ class PurchasesReport():
         buffer.seek(0)
         return buffer
     
-    def fetch(self, from_date, to_date, page=0):
+    def fetch(self, from_date, to_date, category_id=0, page=0):
         self.db.ensure_connection()
         with self.db.conn.cursor() as cursor:
             query = """
-            SELECT s.name, pc.name AS category_name, s.purchase_price, SUM(COALESCE(s.additions,0)) additions 
+            SELECT DATE(s.updated_at) AS report_date, s.name, pc.name AS category_name, s.purchase_price, SUM(COALESCE(s.additions,0)) additions 
             FROM stock s
             LEFT JOIN product_categories pc ON pc.id= s.category_id   
             WHERE (DATE(s.updated_at) BETWEEN DATE(%s) AND DATE(%s)) AND s.shop_id = %s AND s.additions IS NOT NULL AND s.additions>0
-            GROUP BY s.name, pc.name, s.purchase_price
-            ORDER BY pc.name, s.name
             """
             params = [from_date, to_date, current_user.shop.id]
+            
+            if category_id > 0:
+                query = query + " AND s.category_id = %s "
+                params.append(category_id)
+            
+            query = query + """
+            GROUP BY report_date, s.name, pc.name, s.purchase_price
+            ORDER BY report_date DESC, pc.name, s.name
+            """
             
             if page>0:
                 query = query + """
@@ -113,7 +120,7 @@ class PurchasesReport():
             data = cursor.fetchall()
             purchases = []
             for datum in data:                       
-                purchases.append(Purchase(datum[0], datum[1], datum[2], datum[3]))
+                purchases.append(Purchase(datum[0], datum[1], datum[2], datum[3], datum[4]))
 
             return purchases 
          
@@ -122,17 +129,19 @@ class PurchasesReport():
         from_date = to_date = current_date
         page = 1
         download = 0
+        category_id = 0
         
         if request.method == 'GET':   
             try:    
                 from_date = request.args.get('from_date', from_date)
                 to_date = request.args.get('to_date', to_date)
+                category_id = int(request.args.get('category_id', 0))
                 page = int(request.args.get('page', 1))
                 download = int(request.args.get('download', download))
             except Exception as e:
                 print(f"An error occurred: {e}")               
         
-        purchases = self.fetch(from_date, to_date, page) 
+        purchases = self.fetch(from_date, to_date, category_id, page) 
         prev_page = page-1 if page>1 else 0
         next_page = page+1 if len(purchases)==50 else 0
         grand_total =  0
@@ -146,6 +155,6 @@ class PurchasesReport():
         
         product_categories = ProductsCategories(self.db).fetch()
         return render_template('reports/purchases-report.html', page_title='Reports > Purchases', helper=Helper(),
-                               purchases=purchases, grand_total=grand_total, product_categories=product_categories,
-                               current_date=current_date, from_date=from_date, to_date=to_date,
+                               purchases=purchases, grand_total=grand_total, product_categories=product_categories, category_id=category_id,
+                               current_date=current_date, from_date=from_date, to_date=to_date, 
                                 page=page, prev_page=prev_page, next_page=next_page)
--- utils/reports/sales_report.py
+++ utils/reports/sales_report.py
@@ -7,6 +7,7 @@ from reportlab.pdfgen import canvas
 
 from utils.entities import Sale
 from utils.helper import Helper
+from utils.inventory.products_categories import ProductsCategories
 
 class SalesReport():
     def __init__(self, db): 
@@ -89,19 +90,26 @@ class SalesReport():
         buffer.seek(0)
         return buffer
     
-    def fetch(self, from_date, to_date, page=0):
+    def fetch(self, from_date, to_date, category_id=0, page=0):
         self.db.ensure_connection()
         with self.db.conn.cursor() as cursor:
             query = """
-            SELECT be.item_name, pc.name AS category_name, s.selling_price, SUM(COALESCE(be.qty,0)) sold 
+            SELECT DATE(be.created_at) AS report_date, be.item_name, pc.name AS category_name, s.selling_price, SUM(COALESCE(be.qty,0)) sold 
             FROM bill_entries be
             JOIN stock s ON s.id=be.stock_id
             LEFT JOIN product_categories pc ON pc.id= s.category_id   
-            WHERE (DATE(be.created_at) BETWEEN DATE(%s) AND DATE(%s)) AND be.shop_id = %s AND be.bill_id>0
-            GROUP BY be.item_name, pc.name, s.selling_price
-            ORDER BY sold DESC
+            WHERE (DATE(be.created_at) BETWEEN DATE(%s) AND DATE(%s)) AND be.shop_id = %s AND be.bill_id>0            
             """
             params = [from_date, to_date, current_user.shop.id]
+            
+            if category_id > 0:
+                query = query + " AND s.category_id = %s "
+                params.append(category_id)
+            
+            query = query + """
+            GROUP BY report_date, be.item_name, pc.name, s.selling_price
+            ORDER BY report_date DESC, sold DESC
+            """
             
             if page>0:
                 query = query + """
@@ -113,13 +121,14 @@ class SalesReport():
             data = cursor.fetchall()
             sales = []
             for datum in data:                       
-                sales.append(Sale(datum[0], datum[1], datum[2], datum[3]))
+                sales.append(Sale(datum[0], datum[1], datum[2], datum[3], datum[4]))
 
             return sales 
          
     def __call__(self):
         current_date = datetime.now().strftime('%Y-%m-%d')
         from_date = to_date = current_date
+        category_id = 0
         page = 1
         download = 0
         
@@ -127,12 +136,13 @@ class SalesReport():
             try:    
                 from_date = request.args.get('from_date', from_date)
                 to_date = request.args.get('to_date', to_date)
+                category_id = int(request.args.get('category_id', 0))
                 page = int(request.args.get('page', 1))
                 download = int(request.args.get('download', download))
             except Exception as e:
                 print(f"An error occurred: {e}")               
         
-        sales = self.fetch(from_date, to_date, page) 
+        sales = self.fetch(from_date, to_date, category_id, page) 
         prev_page = page-1 if page>1 else 0
         next_page = page+1 if len(sales)==50 else 0
         grand_total =  0
@@ -144,7 +154,8 @@ class SalesReport():
             pdf_file = self.generate_pdf_file(sales, from_date, to_date)
             return send_file(pdf_file, as_attachment=True, download_name=f"Sales_Report_from_{from_date}_to_{to_date}_{page} - {current_user.shop.name}.pdf")
         
+        product_categories = ProductsCategories(self.db).fetch()
         return render_template('reports/sales-report.html', page_title='Reports > Sales', helper=Helper(),
-                               sales=sales, grand_total=grand_total,
+                               sales=sales, grand_total=grand_total, product_categories=product_categories, category_id=category_id,
                                current_date=current_date, from_date=from_date, to_date=to_date,
                                 page=page, prev_page=prev_page, next_page=next_page)

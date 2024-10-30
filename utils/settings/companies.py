1c6fa65df5b0bd4c4376a961e8691c664af88ec7
Dennis Muga <dennis.mutethia1@accesscfa.com>
2024-10-30 22:37:13 +0300

    companies

--- utils/settings/companies.py
+++ utils/settings/companies.py
@@ -1,15 +1,16 @@
 from flask import redirect, render_template, request, url_for
 from flask_login import current_user
 
-from utils.entities import Shop
+from utils.entities import Package, Shop
 from utils.helper import Helper
 
 class Company():
-    def __init__(self, id, name, phone, created_at, license_key, expires_at, package, validity):
+    def __init__(self, id, name, phone, created_at, license_id, license_key, expires_at, package, validity):
         self.id = id
         self.name = name
         self.phone = phone
         self.created_at = created_at
+        self.license_id = license_id
         self.license_key = license_key
         self.expires_at = expires_at
         self.package = package
@@ -23,11 +24,12 @@ class Companies():
         self.db.ensure_connection()
         with self.db.conn.cursor() as cursor:
             query = """
-            SELECT c.id, c.name, u.phone, DATE(c.created_at), l.key, DATE(l.expires_at), p.name, EXTRACT(DAY FROM (l.expires_at - NOW()))
+            SELECT c.id, c.name, u.phone, DATE(c.created_at), l.id, l.key, DATE(l.expires_at), p.name, EXTRACT(DAY FROM (l.expires_at - NOW()))
             FROM companies c
             LEFT JOIN users u ON u.id = c.created_by
             JOIN licenses l ON l.id = c.license_id
             JOIN packages p ON p.id = l.package_id
+            ORDER BY c.id
             """
             #params = [current_user.company.id]
             
@@ -35,19 +37,49 @@ class Companies():
             data = cursor.fetchall()
             companies = []
             for datum in data:   
-                companies.append(Company(datum[0], datum[1], datum[2], datum[3], datum[4], datum[5], datum[6], datum[7]))
+                companies.append(Company(datum[0], datum[1], datum[2], datum[3], datum[4], datum[5], datum[6], datum[7], datum[8]))
 
             return companies 
-       
-    def update(self, shop_id, name, shop_type_id, company_id, location, phone_1, phone_2, paybill, account_no, till_no, updated_by):
+    
+    #def __init__(self, id, name, amount, description, color, validity):
+    def fetch_packages(self):
         self.db.ensure_connection()
         with self.db.conn.cursor() as cursor:
             query = """
-            UPDATE shops 
-            SET name = %s, shop_type_id = %s, company_id = %s, location = %s, phone_1 = %s, phone_2 = %s, paybill = %s, account_no = %s, till_no = %s, updated_at=NOW(), updated_by = %s 
-            WHERE id = %s          
+            SELECT id, name, amount, description, color, validity
+            FROM packages 
+            ORDER BY validity
             """
-            cursor.execute(query, (name.upper(), shop_type_id, company_id, location.upper(), phone_1, phone_2, paybill, account_no, till_no, updated_by, shop_id))
+                        
+            cursor.execute(query)
+            data = cursor.fetchall()
+            packages = []
+            for datum in data:   
+                packages.append(Package(datum[0], datum[1], datum[2], datum[3], datum[4], datum[5]))
+
+            return packages 
+       
+    def update(self, id, name):
+        self.db.ensure_connection()
+        with self.db.conn.cursor() as cursor:
+            query = """
+            UPDATE companies 
+            SET name = %s, updated_at=NOW(), updated_by = %s 
+            WHERE id = %s       
+            """
+            cursor.execute(query, (name.upper(), current_user.id, id))
+            self.db.conn.commit()
+       
+    def update_license(self, id, package_id):
+        package = self.db.get_package_by_id(package_id)
+        self.db.ensure_connection()
+        with self.db.conn.cursor() as cursor:
+            query = """
+            UPDATE licenses 
+            SET package_id = %s, expires_at=NOW() + INTERVAL %s, updated_at=NOW(), updated_by = %s 
+            WHERE id = %s         
+            """
+            cursor.execute(query, (package_id, f'+{package.validity} DAYS', current_user.id, id))
             self.db.conn.commit()
             
     def delete(self, id):
@@ -63,28 +95,21 @@ class Companies():
     def __call__(self):   
         toastr_message = None             
         if request.method == 'POST':       
-            if request.form['action'] == 'update':  
-                name = request.form['name']
-                location = request.form['location']      
-                shop_type_id = request.form['shop_type_id']   
-                company_id = current_user.company.id  
-                phone_1 = request.form['phone_1']       
-                phone_2 = request.form['phone_2']       
-                paybill = request.form['paybill']       
-                account_no = request.form['account_no']       
-                till_no = request.form['till_no']    
-                created_by = current_user.id 
+            if request.form['action'] == 'edit':  
+                id = request.form['id']
+                name = request.form['name'] 
                 
-                shop_id = request.form['shop_id']
-                self.update(shop_id, name, shop_type_id, company_id, location, phone_1, phone_2, paybill, account_no, till_no, created_by)
+                self.update(id, name)
                 toastr_message = f'{name} Updated Successfully'
                                 
-            elif request.form['action'] == 'delete':
-                shop_id = request.form['shop_id']
-                self.delete(shop_id)
-                toastr_message = f'Shop Deleted Successfully'
+            elif request.form['action'] == 'renew':
+                id = request.form['id']
+                package_id = request.form['package_id']
+                self.update_license(id, package_id)
+                toastr_message = 'License Renewed Successfully'
         
         companies = self.fetch() 
+        packages = self.fetch_packages()
             
         return render_template('settings/companies.html', page_title='Companies', helper=Helper(),
-                               companies=companies, toastr_message=toastr_message )
+                               companies=companies, packages=packages, toastr_message=toastr_message )
